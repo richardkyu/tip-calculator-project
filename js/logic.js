7af1c7778a1cb3fb82126311cf3e00f353723d9c
richardkyu <richard.k.yu@vanderbilt.edu>
2019-06-02 12:49:50 -0400

    Added final project requirement - splitting option and error handling for it.

--- js/logic.js
+++ js/logic.js
@@ -51,14 +51,19 @@ $("select").on('change', function(){
 var isFloat = function(n) {/*console.log(parseFloat(n),n);*/ return parseFloat(n) == n };
 /*Script to handle the intake of information such as the tip amount and price of the meal.
 This script will handle the calculations and necessary type conversions (string, float) as well. */
-
+var price_outer
+var tip_outer
+var total_outer
     
     $("#takeinput").on("click", function() {
         $('#userNotice').html("")
-        
+        $('#splitPrice').html('')
+        $('#splitTip').html('')
+        $('#splitTotal').html('')
         var price = parseFloat(parseFloat($('#Price').val()).toFixed(2))
         var tip = parseFloat($('#Tip').val()).toFixed(2)
         var tip_calc = $('#Tip').val() /100 *price
+
         if ($("#paymentMethod option:selected").text()==='Credit'){
             var cardNo = parseFloat($('#cardNo').val()).toString()
             var CVV = parseFloat($('#CVV').val()).toString()
@@ -105,7 +110,9 @@ This script will handle the calculations and necessary type conversions (string,
         else if (price<0 || tip <0){
             $('#userNotice').html("<font color ='red'>Please do not enter negative numbers!</br>We will take the absolute value of your provided values for calculation.</font>")
         price = Math.abs(price)
+        price_outer = price
         tip_calc = Math.abs(tip_calc)
+        tip_outer = tip_calc
         //Continue to process calculations even if negative.
         var tip_amount = parseFloat(tip_calc.toFixed(2))
 
@@ -113,6 +120,8 @@ This script will handle the calculations and necessary type conversions (string,
         $('.tipAmount').html(tip_amount)
         $('#percent').html(Math.abs(parseFloat(tip)).toFixed(2)+'%')
         $('.odometer_total').html((tip_amount+price).toFixed(2))
+
+        total_outer = tip_amount+price
         }
 
         //4. If the user is asking for the price on a free meal.
@@ -128,22 +137,61 @@ This script will handle the calculations and necessary type conversions (string,
         //If all error conditions are passed, populate the appropriate fields.
         else{
         var tip_amount = parseFloat(tip_calc.toFixed(2))
+        price_outer = price
+        tip_outer = tip_calc
 
         $('.priceAmount').html(price)
         $('.tipAmount').html(tip_amount)
         $('#percent').html(Math.abs(parseFloat(tip)).toFixed(2)+'%')
         $('.odometer_total').html((tip_amount+price).toFixed(2))
+        total_outer = tip_amount+price
         }
+    });
+    //Function to add the calculations for split.
+    
+    $("#splitNumber").on("click", function() {
+        //console.log($('#split').val())
+
+
+        if ($('.odometer_total').text()==0){
+            $('#splitNotify').html("<font color = 'red'>You need calculate the bill before splitting it.</font>")
+            
+            //Make notification disappear after one second.
+            setTimeout(function () {
+                $('#splitNotify').html('')
+            },1000);
+        }
+        
+        else if ($('#split').val()===''){
+            $('#splitNotify').html("<font color = 'red'>Please enter in a number.</font>")
+            
+            //Make notification disappear after one second.
+            setTimeout(function () {
+                $('#splitNotify').html('')
+            },1500);
+        }
+
+        //Calculate splits across price, tip, and total if error handling succeeds.
+        else{
+            //console.log(price_outer, $('#split').val())
+            $('#splitPrice').html("Price split by "+$('#split').val() +" people: "+(price_outer/parseFloat($('#split').val())).toFixed(2))
+            $('#splitTip').html("Tip split by "+$('#split').val()+" people: "+(tip_outer/parseFloat($('#split').val())).toFixed(2))
+            $('#splitTotal').html("Total split by "+$('#split').val()+" people: "+(total_outer/parseFloat($('#split').val())).toFixed(2))
+        }        
+
+        
+        //console.log($('#split').val())
     });
 
 });
+
 
 //Function to check if credit card is valid.
 function testCreditCard() {
     myCardNo = $('#cardNo').val();
     myCardType = document.getElementById('CardType').value;
     if (checkCreditCard(myCardNo, myCardType)) {
-        alert("Credit card has a valid format")
+        $('#creditCardAlert').html("<font color ='green'>Accepted.</font>")
     } else {
         $('#creditCardAlert').html("<font color ='red'>The credit card number you have entered is not valid.</font>")
         //alert(ccErrors[ccErrorNo])
